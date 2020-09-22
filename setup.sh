b9947f2687e74c7732d115be5604cc6b6c287d2b
strogiyotec <almas337519@gmail.com>
2020-09-21 20:29:53 -0700

    format code

--- setup.sh
+++ setup.sh
@@ -6,15 +6,15 @@
 
 mvn clean package
 echo "Native image compiled"
-cd target 
+cd target
 jfill_version=$(./jfill -v | grep -oP "([0-9.])+")
 release_name="jfill-${jfill_version}"
-mkdir $release_name
+mkdir ${release_name}
 echo "Start upx compression"
 upx_target="${release_name}/jfill"
-upx -o $upx_target jfill
+upx -o ${upx_target} jfill
 echo "upx file ${upx_target} is ready"
 zipped_file="${release_name}.zip"
-zip -r $zipped_file $release_name
+zip -r ${zipped_file} ${release_name}
 echo "${zipped_file} release is ready"
 
